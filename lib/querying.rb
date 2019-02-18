70acfbb0844ec7ea0090b2ba7304774762ac45d5
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-18 08:07:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY series.title 
+  GROUP BY series.title
   LIMIT 1;"
 end
 
